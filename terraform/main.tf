9e6e08cac2a67259e2161a1240c2dcf11bff661e
siham-bkz <lakhdaris021@gmail.com>
2025-11-30 01:26:58 +0100

    update main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,33 @@
-resource "aws_sns_topic" "aws-billing" {
+resource "aws_sns_topic" "aws_billing" {
   name = "aws-billing-topic"
+}
+
+
+resource "aws_sns_topic_subscription" "billing_email" {
+  topic_arn = aws_sns_topic.aws_billing.arn
+  protocol  = "email"
+  endpoint  = "boukhobza.siham22@gmail.com"  # Your email
+  
+  # Wait for email confirmation
+  confirmation_timeout_in_minutes = 10
+}
+
+
+
+
+resource "aws_cloudwatch_metric_alarm" "billing_alarm" {
+  alarm_name                = "aws-watching-alarm"
+  alarm_description         = "Monitor AWS monthly charges exceeding budget"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = "EstimatedCharges"
+  namespace                 = "AWS/Billing"
+  period                    = "21600"
+  statistic                 = "maximum"
+  threshold                 = 50
+  alarm_actions = [aws_sns_topic.aws_billing.arn]
+
+  dimensions = {
+    Currency = "USD"  # ← REQUIRED for billing alarms
+  }
 }
